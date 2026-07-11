34b5f7f5f47627bd898ee5fabacb8541b379fbf3
sanjayrockerz <myteamcreations09@gmail.com>
2026-07-11 23:24:24 +0530

    Fix core-js PDF bundle dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import 'core-js/stable'
+import 'core-js/features/web.dom-collections.iterator'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
